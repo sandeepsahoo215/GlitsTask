fda807453b9f4d3e4b529248024dc95899d61047
sandeepsahoo215 <90467373+sandeepsahoo215@users.noreply.github.com>
2023-01-11 15:21:29 +0530

    Update app.component.ts

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,34 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ApiService } from './service/api.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'app';
+  data:any;
+  dataList:number = 0;
+  p: number = 1;
+  constructor(private api:ApiService) { }
+
+
+  ngOnInit(): void {
+    this.listData();
+  }
+listData() {
+  this.api.getListData(this.p).subscribe((res:any)=>{
+    this.data = res.data;
+    this.dataList = res.total;
+    console.log(res.total)
+  })
+}
+
+page(event:number) {
+  this.p = event;
+  this.listData()
+}
+
+  
 }
